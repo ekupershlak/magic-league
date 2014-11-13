b99a0b7d320d0b43cda86936d7fbdc85fd15ccee
Chris Connett <chris@connett.net>
2014-11-13 13:45:51 -0500

    It does three rounds by backtracking with bigger pair ups.

--- swiss.py
+++ swiss.py
@@ -1,63 +1,111 @@
+import copy
+import collections
+import pdb
 import gspread
 import itertools
 import random
 import sys
+import xpermutations
 
 sheets_account = 'chrisconnett@gmail.com'
 sheets_password = 'fyiyjhehvjmrongv'
 sheets_spreadsheet = 'magic-ny KTK Sealed League'
 n = 3
+limit = 40320
 
-session = gspread.login(sheets_account, sheets_password)
-spreadsheet = session.open(sheets_spreadsheet)
+def take(n, iterable):
+    "Return first n items of the iterable as a list"
+    return list(itertools.islice(iterable, n))
 
-standings = spreadsheet.worksheet('Standings')
-names = standings.col_values(2)[1:]
-scores = [int(s) for s in standings.col_values(4)[1:]]
+class CouldNotPairError(Exception):
+  """Could not pair."""
 
-previous_pairings = set()
-partial_scores = dict.fromkeys(names, 0)
-total_mismatch = dict.fromkeys(names, 0)
+def fetch():
+  session = gspread.login(sheets_account, sheets_password)
+  spreadsheet = session.open(sheets_spreadsheet)
 
-for i in range(1, n + 1):
-  cycle = spreadsheet.worksheet('Cycle {}'.format(i))
-  a = cycle.col_values(3)[1:]
-  b = cycle.col_values(4)[1:]
-  winners = cycle.col_values(7)[1:]
-  for pa, pb, winner in zip(a, b, winners):
-    total_mismatch[pa] += partial_scores[pb] - partial_scores[pa]
-    total_mismatch[pb] += partial_scores[pa] - partial_scores[pb]
-    partial_scores[pa] += {
-      pa: 3,
-      "Didn't play:{}-{}".format(pa, pb): 1,
-      pb: 0}[winner]
-    partial_scores[pb] += {
-      pb: 3,
-      "Didn't play:{}-{}".format(pa, pb): 1,
-      pa: 0}[winner]
-  previous_pairings |= set(zip(a, b))
-  previous_pairings |= set(zip(b, a))
+  standings = spreadsheet.worksheet('Standings')
+  names = standings.col_values(2)[1:]
+  scores = [int(s) for s in standings.col_values(4)[1:]]
 
-#assert partial_scores == dict(zip(names, scores))
+  previous_pairings = set()
+  partial_scores = dict.fromkeys(names, 0)
+  total_mismatch = dict.fromkeys(names, 0)
 
-groups = [list(group) for score, group in
-          itertools.groupby(sorted(zip(scores, names), reverse=True))]
+  for i in range(1, n + 1):
+    cycle = spreadsheet.worksheet('Cycle {}'.format(i))
+    a = cycle.col_values(3)[1:]
+    b = cycle.col_values(4)[1:]
+    winners = cycle.col_values(7)[1:]
+    for pa, pb, winner in zip(a, b, winners):
+      total_mismatch[pa] += partial_scores[pb] - partial_scores[pa]
+      total_mismatch[pb] += partial_scores[pa] - partial_scores[pb]
+      partial_scores[pa] += {
+        pa: 3,
+        "Didn't play:{}-{}".format(pa, pb): 1,
+        pb: 0}[winner]
+      partial_scores[pb] += {
+        pb: 3,
+        "Didn't play:{}-{}".format(pa, pb): 1,
+        pa: 0}[winner]
+    previous_pairings |= set(zip(a, b))
+    previous_pairings |= set(zip(b, a))
 
-pairings = []
+    groups = [list(group) for score, group in
+              itertools.groupby(sorted(zip(scores, names), reverse=True),
+                                key=lambda (s, n): s)]
+  return groups, previous_pairings
 
-## while groups:
-##   current = groups.pop()
-##   random.shuffle(current)
-##   while len(current) > 1:
-##     pairing = (current[0], current[1])
-##     if pairing has already played:
-##       search group for someone [0] has not played then someone [1] has not played
-##       if none go back to shuffling. in the limit, they will be in the first pairing
-##       if this is the first pairing, pair down at random
-## pairings.append()
+def pop_pairup(groups):
+  for group in groups:
+    if group:
+      pairup = random.choice(group)
+      group.remove(pairup)
+      return pairup
+  raise CouldNotPairError()
+
+def pair(groups, previous_pairings):
+  if not groups:
+    return []
+  groups = collections.deque(copy.deepcopy(groups))
+  pairings = []
+  current = groups.popleft()
+  while True:
+    if len(current) % 2 == 1:
+      pairup = pop_pairup(groups)
+      current.append(pairup)
+    random.shuffle(current)
+    print 'permuting {} things'.format(len(current))
+    for permutation in take(limit, xpermutations.xpermutations(current)):
+      group_pairings = []
+      while permutation:
+        pa = permutation[0]
+        pb = permutation[1]
+        if (pa[1], pb[1]) not in previous_pairings:
+          group_pairings.append((pa, pb))
+          permutation = permutation[2:]
+        else:
+          break
+      if len(group_pairings) == len(current) / 2:
+        try:
+          pairings.extend(group_pairings + pair(groups, previous_pairings))
+          return pairings
+        except CouldNotPairError:
+          pass
+    if groups:
+      super_pairup = pop_pairup(groups)
+      current.append(super_pairup)
+    else:
+      raise CouldNotPairError('Could not pair')
+
+def pair3(groups, previous_pairings):
+  previous_pairings = copy.deepcopy(previous_pairings)
+  for i in range(1, 4):
+    pairings = pair(groups, previous_pairings)
+    yield pairings
+    previous_pairings |= set((pa, pb) for (sa, pa), (sb, pb) in pairings)
+    previous_pairings |= set((pb, pa) for (sa, pa), (sb, pb) in pairings)
 
 
-##   if odd group:
-##     if bottom player is ineligible for pair-down:
-##       reshuffle
-##     grab a random player eligible for pair-up
+def main():
+  groups, previous_pairings = fetch()
