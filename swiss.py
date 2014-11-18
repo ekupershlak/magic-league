becdcefcdb318f29adbecc66db253c1489991058
Chris Connett <chris@connett.net>
2014-11-17 23:58:42 -0500

    Working but slow in z3.

--- swiss.py
+++ swiss.py
@@ -22,6 +22,9 @@ def take(n, iterable):
     "Return first n items of the iterable as a list"
     return list(itertools.islice(iterable, n))
 
+def odd(n):
+  return n % 2 == 1
+
 class CouldNotPairError(Exception):
   """Could not pair."""
 
@@ -123,104 +126,132 @@ def pair3(groups, previous_pairings, total_mismatch):
     acc.append(pairings)
   return sum(mismatch**2 for mismatch in total_mismatch.values()), acc
 
-def make_slots(env, n_players, r_rounds):
+def MakeSlots(s, n_players, r_rounds):
   """Creates output pairing variables."""
-  int_type = msat_get_integer_type(env)
-  zero = msat_make_number(env, '0')
-  upper_bound = msat_make_number(env, str(n_players - 1))
   slots = []
   for r in range(r_rounds):
     round_slots = []
     slots.append(round_slots)
     for n in range(n_players):
-      slot = msat_declare_function(env, 'r_{}-{}'.format(r, n), int_type)
-      # Bounded
-      msat_assert_formula(env, msat_make_leq(env, zero, slot))
-      msat_assert_formula(env, msat_make_leq(env, slot, upper_bound))
+      slot = z3.Int('r_{},{}'.format(r, n))
       round_slots.append(slot)
-      # Sorted
-      if n != 0:
-        msat_assert_formula(
-          env, msat_make_leq(env, round_slots[n], round_slots[n - 1]))
+      # Bounded
+      s.add(0 <= slot)
+      s.add(slot < n_players)
     # Distinct
-    msat_assert_formula(
-      env, msat_from_string(env, '(distinct {})'.format(
-        ' '.join('r_{}-{}'.format(r, n) for n in range(n_players)))))
-
+    s.add(z3.Distinct(*round_slots))
   return slots
 
-def make_score_function(env, scores):
+def MakeScoreFunction(s, scores):
   """Creates match points score mapping function."""
-  int_type = msat_get_integer_type(env)
-  score_function_type = msat_get_function_type(env, [int_type], int_type)
-  func = msat_declare_function(env, 'score', score_function_type)
+  f = z3.Function('score', z3.IntSort(), z3.IntSort())
   for player_id, score in scores.items():
-    msat_assert_formula(msat_make_equal(
-      env, msat_make_number(env, str(player_id)),
-      msat_make_number(env, str(score))))
-  return func
+    s.add(f(player_id) == score)
+  return f
 
-def make_played_function(env, previous_pairings):
-  int_type = msat_get_integer_type(env)
-  bool_type = msat_get_bool_type(env)
-  played_function_type = msat_get_function_type(
-    env, [int_type, int_type], bool_type)
-  true = msat_make_true(env)
-  id_mapping = player_id_mapping(previous_pairings)
+def SortSlotsByScore(s, slots, score):
+  for r, round_slots in enumerate(slots):
+    for n, slot in enumerate(round_slots):
+      # Sorted
+      if n != 0:
+        s.add(score(round_slots[n]) <= score(round_slots[n - 1]))
 
-  func = msat_declare_function(env, 'played', pair_function_type)
+def MakePlayedFunction(s, previous_pairings, players):
+  played = z3.Function('played', z3.IntSort(), z3.IntSort(), z3.BoolSort())
+
   for (pa, pb) in previous_pairings:
-    msat_assert_formula(
-      env, msat_assert_equal(env, true, msat_make_uf(
-        env, func, [msat_make_number(env, str(id_mapping[pa])),
-                    msat_make_number(env, str(id_mapping[pb]))])))
-    msat_assert_formula(
-      env, msat_assert_equal(env, true, msat_make_uf(
-        env, func, [msat_make_number(env, str(id_mapping[pb])),
-                    msat_make_number(env, str(id_mapping[pa]))])))
-  return func
+    #print 'Added', played(players[pa], players[pb])
+    s.add(played(players[pa], players[pb]))
+    #print 'Added', played(players[pb], players[pa])
+    s.add(played(players[pb], players[pa]))
+  #a, b = z3.Ints('a b')
+  #s.add(z3.ForAll([a, b], played(a, b) == played(b, a)))
+  return played
 
-def no_repeat_matches(env, slots, pair_func):
-  false = msat_make_false(env)
+def NoRepeatMatches(s, slots, played):
   for r, round_slots in enumerate(slots):
     for n, slot in enumerate(round_slots):
       if odd(n):
-        msat_assert_formula(
-          env, msat_make_equal(env, false, msat_make_uf(
-            env, pair_func, [slots[r][n-1], slots[r][n]])))
+        s.add(z3.Not(played(slots[r][n-1], slots[r][n])))
     if odd(r) and odd(len(slots[0])):
-      msat_assert_formula(
-        env, msat_make_equal(env, false, msat_make_uf(
-          env, pair_func, [slots[r-1][-1], slots[r][-1]])))
+      s.add(z3.Not(played(slots[r-1][-1], slots[r][-1])))
 
-def odd(n):
-  return n % 2 == 1
-
-def no_repeat_byes(env, slots, previous_pairings):
+def NoRepeatByes(s, slots, previous_pairings, players):
   previously_byed = [player for (player, bye) in previous_pairings
                      if bye == BYE]
-  id_mapping = player_id_mapping(previous_pairings)
   for player in previously_byed:
-    msat_assert_formula(env, msat_make_not(
-      env, msat_make_equal(env, msat_make_number(
-        env, str(id_mapping[player])), slots[-1][-1])))
+    s.add(slots[-1][-1] != players[player])
 
 def player_id_mapping(previous_pairings):
-  all_names = (set(pair[0] for pair in previous_pairings) +
+  all_names = (set(pair[0] for pair in previous_pairings) |
                set(pair[1] for pair in previous_pairings))
   all_names -= set([BYE])
   return dict(zip(sorted(all_names), itertools.count()))
 
-def metric1(env):
-  for player, number in player_id_mapping.items():
+# Metric 1
+def PerPlayerAbsoluteMismatchSumSquared(s, slots, players, score_func):
+  def Abs(x):
+    return z3.If(x >= 0, x, -x)
 
+  mismatches = [
+    z3.Array('abs_mismatch_' + str(r), z3.IntSort(), z3.IntSort())
+    for r in range(len(slots))]
+  for round_slots, round_mismatch in zip(slots, mismatches):
+    for n, slot in enumerate(round_slots):
+      if odd(n):
+        player = slot
+        opponent = round_slots[n - 1]
+        s.add(round_mismatch[player] == round_mismatch[opponent] ==
+              Abs(score_func(player) - score_func(opponent)))
+    if odd(len(round_slots)):
+      s.add(round_mismatch[-1] == score_func(round_slots[-1]))
+
+  def AbsoluteMismatchSum(index):
+    return z3.Sum(*[round_mismatch[index] for round_mismatch in mismatches])
+
+  return z3.Sum(*[AbsoluteMismatchSum(player_id) ** 2
+                  for player_id in players.values()])
+
+def PerPlayerSquaredSumMismatch(s, slots, players, score_func):
+  pass
 
 def main():
-  groups, previous_pairings, total_mismatch, player_mismatch = fetch()
-  id_mapping = player_id_mapping(previous_pairings)
-  scores = {id_mapping[player_name]: score
-            for player_name, score in itertools.chain(*groups)}
-  best = min(pair3(groups, previous_pairings, tm) for i in range(30))
-  for round in best[0]:
-    for (sa, pa), (sb, pb) in round:
-        print '{}\t{}'.format(pa, pb)
+  ## best = min(pair3(groups, previous_pairings, tm) for i in range(30))
+  ## for round in best[0]:
+  ##   for (sa, pa), (sb, pb) in round:
+  ##       print '{}\t{}'.format(pa, pb)
+  pass
+
+import cPickle
+try:
+  file('dat')
+except IOError:
+  cPickle.dump(fetch(), file('dat', 'w'))
+groups, previous_pairings, total_mismatch, player_mismatch = cPickle.load(
+  file('dat'))
+
+
+players = player_id_mapping(previous_pairings)
+scores = {players[name]: score for score, name in itertools.chain(*groups)}
+
+
+s = z3.Solver()
+slots = MakeSlots(s, len(players), 1)
+score = MakeScoreFunction(s, scores)
+# SortSlotsByScore(s, slots, score)
+played = MakePlayedFunction(s, previous_pairings, players)
+NoRepeatMatches(s, slots, played)
+NoRepeatByes(s, slots, previous_pairings, players)
+metric1 = PerPlayerAbsoluteMismatchSumSquared(s, slots, players, score)
+
+def PrintModel(slots, players, model):
+  reverse_players = {number: name for name, number in players.items()}
+  for r, round_slots in enumerate(slots):
+    print 'Round', r + 1
+    for n, slot in enumerate(round_slots):
+      if odd(n):
+        print '{:>20} vs. {:<20}'.format(
+          reverse_players[model.evaluate(slot).as_long()],
+          reverse_players[model.evaluate(round_slots[n - 1]).as_long()])
+  print
+  print 'Badness:', model.evaluate(metric1)
