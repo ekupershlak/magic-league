2d271879768a55623e8385b7e504e9452fc60153
Chris Connett <cjc@google.com>
2018-05-17 15:43:30 -0400

    Add short versions to flags.

--- swiss.py
+++ swiss.py
@@ -36,11 +36,13 @@ flags.add_argument(
 )
 flags.add_argument(
     '--write_pairings',
+    '-w',
     action='store_true',
     help='whether to write the pairings to the spreadsheet',
 )
 flags.add_argument(
     '--time_limit',
+    '-t',
     metavar='n',
     type=int,
     default=600,
