64437a6c242207c7e29205a350fe0153b5398414
Chris Connett <cjc@google.com>
2018-08-06 15:32:08 -0400

    Fix scope for gspread.

--- password.py
+++ password.py
@@ -3,7 +3,10 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds']
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive',
+]
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name(
     'credentials.json', scope)
