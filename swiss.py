da3894c28f71aca7a8eda274f0917d92847b0bad
Chris Connett <cjc@google.com>
2019-06-19 16:44:48 -0400

    Fix format string.

--- swiss.py
+++ swiss.py
@@ -315,7 +315,7 @@ def Main():
   pairings = pairer.MakePairings(random_pairings=FLAGS.cycle in (1,))
   PrintPairings(pairings)
   t = time.time() - start
-  print(f'Finished in {t // 60}m{t % 60:.2f}s wall time.')
+  print(f'Finished in {t // 60:d}m{t % 60:.1f}s wall time.')
   try:
     os.mkdir('pairings')
   except FileExistsError:
