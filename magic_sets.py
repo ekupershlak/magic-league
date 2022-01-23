c4aa42556ad0e3207c776ea3728e8dfedd367449
Chris Connett <cjc@google.com>
2022-01-23 10:34:36 -0500

    Set updates.

--- magic_sets.py
+++ magic_sets.py
@@ -4,4 +4,10 @@ names = {
     'THB': 'Theros Beyond Death',
     'IKO': 'Ikoria: Lair of Behemoths',
     'M21': 'Core Set 2021',
+    'ZNR': 'Zendikar Rising',
+    'KHM': 'Kaldheim',
+    'STX': 'Strixhaven',
+    'AFR': 'Forgotten Realms',
+    'MID': 'Innistrad Midnight Hunt',
+    'VOW': 'Innistrad Crimson Vow',
 }
